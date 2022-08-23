f1472dc175eb8f2f955cc22f91ff0b40e8152568
BlessingLife_Studio <99802662+Blessing-Studio@users.noreply.github.com>
2022-08-23 13:02:32 +0800

    Add files via upload

---
+++ GameApiTools/Mihoyo/ListTools.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Channels;
+using GameApiTools.Module.Interface;
+using System;
+using GameApiTools.Module.Constants;
+using GameApiTools.Module.Models.MiHoYo;
+using Newtonsoft.Json;
+
+namespace GameApiTools.Mihoyo;
+
+public class ListTools :  IHttpSend,IApiSend<SignInListModels>,IApiListSend<List<EmoticonModels>>
+{
+    public string HttpGet(string uri)
+    {
+        HttpClient hc = new HttpClient();
+        return hc.GetStringAsync(uri).Result;
+    }
+    
+    public string HttpPost(string uri, string postheader)
+    {
+        return "Post";
+    }
+
+    public SignInListModels Send(string? gametype = ApiConstant.MiHoYoSignInApi)
+    {
+        return SendAsync(gametype).Result;
+    }
+
+    public async Task<SignInListModels> SendAsync(string? gametype = ApiConstant.MiHoYoSignInApi)
+    {
+        return await Task.Run(() =>
+        {
+            if (gametype is not ApiConstant.MiHoYoSignInApi)
+                throw new ArgumentException("错误的api，亦或是此方法不适用的api");
+            var json = SendJson(gametype);
+            var v = JsonConvert.DeserializeObject<SignInListModels>(json);
+            if (v is null || json is null)
+                throw new NullReferenceException("哎嘿，数据返回值为空");
+            return v;
+        });
+    }
+
+    public string SendJson(string? gametype)
+    {
+        return HttpGet(gametype);
+    }
+    /// <summary>
+    /// 表情包列表拉取方法
+    /// </summary>
+    /// <param name="gametype"></param>
+    /// <returns></returns>
+    public List<EmoticonModels> ListSend(string? gametype)
+    {
+        return ListSendAsync(gametype).Result;
+    }
+
+    /// <summary>
+    /// 表情包列表异步拉取方法
+    /// </summary>
+    /// <param name="gametype"></param>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException"></exception>
+    public async Task<List<EmoticonModels>> ListSendAsync(string? gametype)
+    {
+        return await Task.Run((() =>
+        {
+            List<EmoticonModels> em = new();
+            var json = SendJson(gametype);
+            var refs = JsonConvert.DeserializeObject<Root>(json);
+            foreach (var i in refs.data.list)
+                em.Add(i);
+                if (refs is null || json is null)
+                throw new NullReferenceException("哎嘿，数据返回值为空");
+            return em;
+        }));
+    }
+}
---
+++ GameApiTools/Mihoyo/SignInTools.cs
@@ -0,0 +1,141 @@
+﻿using GameApiTools.Module.Constants;
+using GameApiTools.Module.Helper;
+using GameApiTools.Module.Models.MiHoYo;
+
+namespace GameApiTools.Mihoyo;
+
+/// <summary>
+/// 签到工具类
+/// </summary>
+public class SignInTools
+{
+            private static readonly string Referer =
+            $"{ApiConstant.ReferBaseUrl}?bbs_auth_required=true&act_id={ApiConstant.ActivityId}&utm_source=bbs&utm_medium=mys&utm_campaign=icon";
+
+        private readonly string cookie;
+
+        public SignInTools(string cookie)=> this.cookie = cookie;
+        /// <summary>
+        /// 签到通用请求器
+        /// </summary>
+        private Requester SignInRequester
+        {
+            get
+            {
+                return new(new RequestOptions
+                {
+                    {"DS", DynamicSecretProvider.Create() },
+                    {"x-rpc-app_version", DynamicSecretProvider.AppVersion },
+                    {"User-Agent", RequestOptions.CommonUA2281 },
+                    {"x-rpc-device_id", RequestOptions.DeviceId },
+                    {"Accept", RequestOptions.Json },
+                    {"x-rpc-client_type", "5" },
+                    {"Referer", Referer },
+                    {"Cookie", cookie },
+                    {"X-Requested-With", RequestOptions.Hyperion }
+                });
+            }
+        }
+
+        /// <summary>
+        /// 获取当前签到信息
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<SignInInfo?> GetSignInInfoAsync(UserGameRole role, CancellationToken cancellationToken = default)
+        {
+            Requester requester = new(new RequestOptions
+            {
+                {"Accept", RequestOptions.Json },
+                {"User-Agent",RequestOptions.CommonUA2101 },
+                {"x-rpc-device_id", RequestOptions.DeviceId },
+                {"Referer", Referer },
+                {"Cookie", cookie },
+                {"X-Requested-With", RequestOptions.Hyperion }
+            });
+            string query = $"act_id={ApiConstant.ActivityId}&region={role.Region}&uid={role.GameUid}";
+            ResponseModels<SignInInfo>? resp = await requester
+                .GetAsync<SignInInfo>($"{ApiConstant.ApiTakumi}/event/bbs_sign_reward/info?{query}", cancellationToken)
+                .ConfigureAwait(false);
+            return resp?.Data;
+        }
+
+        /// <summary>
+        /// 获取当前补签信息
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<ReSignInfo?> GetReSignInfoAsync(UserGameRole role, CancellationToken cancellationToken = default)
+        {
+            Requester requester = new(new RequestOptions
+            {
+                {"Accept", RequestOptions.Json },
+                {"User-Agent",RequestOptions.CommonUA2101 },
+                {"Referer", Referer },
+                {"Cookie", cookie },
+                {"X-Requested-With", RequestOptions.Hyperion }
+            });
+            string query = $"act_id={ApiConstant.ActivityId}&region={role.Region}&uid={role.GameUid}";
+            ResponseModels<ReSignInfo>? resp = await requester
+                .GetAsync<ReSignInfo>($"{ApiConstant.ApiTakumi}/event/bbs_sign_reward/resign_info?{query}", cancellationToken)
+                .ConfigureAwait(false);
+            return resp?.Data;
+        }
+
+        /// <summary>
+        /// 签到
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<(bool IsOk, string Result)> SignInAsync(UserGameRole role, CancellationToken cancellationToken = default)
+        {
+            var data = new { act_id = ApiConstant.ActivityId, region = role.Region, uid = role.GameUid };
+            Requester requester = SignInRequester;
+            ResponseModels<SignInResult>? resp = await requester
+                .PostAsync<SignInResult>($"{ApiConstant.ApiTakumi}/event/bbs_sign_reward/sign", data, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (resp == null)
+            {
+                return (false, "请求失败");
+            }
+            else
+            {
+                if (resp.Data == null)
+                {
+                    return resp.ReturnCode switch
+                    {
+                        0 => (true, "签到成功"),
+                        -5003 => (true, resp.Message!),
+                        _ => (false, resp.Message!),
+                    };
+                }
+                else
+                {
+                    if (resp.Data.RiskCode == 0 && resp.Data.Success == 0)
+                    {
+                        return (true, "签到成功");
+                    }
+                    else
+                    {
+                        return (false, $"[RiskCode: {resp.Data.RiskCode} | Success: {resp.Data.Success}] 账号受到风控!");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 补签
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<string> ReSignAsync(UserGameRole role, CancellationToken cancellationToken = default)
+        {
+            var data = new { act_id = ApiConstant.ActivityId, region = role.Region, uid = role.GameUid };
+            Requester requester = SignInRequester;
+            ResponseModels<SignInResult>? resp = await requester
+                .PostAsync<SignInResult>($"{ApiConstant.ApiTakumi}/event/bbs_sign_reward/resign", data, cancellationToken)
+                .ConfigureAwait(false);
+            return resp is null ? "签到失败" : resp.Message ?? "签到成功";
+        }
+}
