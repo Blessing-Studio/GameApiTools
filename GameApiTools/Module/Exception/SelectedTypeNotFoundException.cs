651272706bd932a373f58a55daaa30c16e09d2b9
BlessingLife_Studio <99802662+Blessing-Studio@users.noreply.github.com>
2022-08-23 13:03:34 +0800

    Delete SelectedTypeNotFoundException.cs

--- GameApiTools/Module/Exception/SelectedTypeNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameApiTools.Module.Exception;
-
-/// <summary>
-/// 未找到选择api类型的异常
-/// </summary>
-public class SelectedTypeNotFoundException : System.Exception
-{
-    public SelectedTypeNotFoundException(string message) : base(message) => Console.WriteLine(message);
-}
